7cd88bbde26b7573a0278c2d79a4d113ce5103e2
Ian McGillivary <wiian16@icloud.com>
2024-12-25 12:56:27 -0500

    Guild only logging and changed ctx.message.author to ctx.author

--- incident_tracker.py
+++ incident_tracker.py
@@ -21,24 +21,38 @@ async def on_ready():
     logger.info(f"Logged in as {bot.user}")
 
 @bot.command(name="track")
+@commands.guild_only()
 async def track_command(ctx, *args):
-    logger.info(f"Track command called by user {ctx.message.author}")
+    logger.info(f"Track command called by user {ctx.author}")
 
 @bot.command(name="report")
+@commands.guild_only()
 async def report_command(ctx, *args):
-    logger.info(f"Report command called by user {ctx.message.author}")
+    logger.info(f"Report command called by user {ctx.author}")
 
 @bot.command(name="reset")
+@commands.guild_only()
 async def reset_command(ctx, *args):
-    logger.info(f"Report command called by user {ctx.message.author}")
+    logger.info(f"Report command called by user {ctx.author}")
 
 @bot.command(name="remove")
+@commands.guild_only()
 async def remove_command(ctx, *args):
-    logger.info(f"Remove command called by user {ctx.message.author}")
+    logger.info(f"Remove command called by user {ctx.author}")
 
 @bot.command(name="list")
+@commands.guild_only()
 async def list_command(ctx):
-    logger.info(f"List command called by user {ctx.message.author}")
+    logger.info(f"List command called by user {ctx.author}")
+
+@bot.event
+async def on_command_error(ctx, error):
+    logger.debug(f"{error} received from command {ctx.command}")
+
+    # Handle private message errors
+    if isinstance(error, commands.errors.NoPrivateMessage):
+        logger.info(f"Command '{ctx.command}' incorrectly used in private message by user '{ctx.author}'")
+        await ctx.send("Sorry! This command can only be used in servers.")
 
 # Getting Discord token
 logger.debug("Loading environment variables")
